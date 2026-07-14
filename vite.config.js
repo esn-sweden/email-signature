a173abb5daefcb995cb37eaab666d87d6657f0e4
Elias Jollivet <63159238+Lurbyte@users.noreply.github.com>
2026-07-14 18:00:07 +0200

    Upgrade dependencies
    Silencing sass deprecation warnings with vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,27 @@
-import { defineConfig } from 'vite'
-import yaml from '@rollup/plugin-yaml';
+import { defineConfig } from "vite";
+import yaml from "@rollup/plugin-yaml";
 
 export default defineConfig({
   server: {
     proxy: {
-      '/api': {
-        target: 'https://accounts.esn.org/',
+      "/api": {
+        target: "https://accounts.esn.org/",
         changeOrigin: true,
-      }
-    }
+      },
+    },
   },
-  plugins: [yaml()]
-})
+  plugins: [yaml()],
+  css: {
+    preprocessorOptions: {
+      scss: {
+        silenceDeprecations: [
+          "mixed-decls",
+          "color-functions",
+          "global-builtin",
+          "import",
+          "if-function",
+        ],
+      },
+    },
+  },
+});
